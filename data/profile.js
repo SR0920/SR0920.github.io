1733a9711f8f9c1efbdea8989ea5694f5a5da818
SII--Sunray <129134456+SR0920@users.noreply.github.com>
2026-04-30 17:26:57 +0800

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -21,8 +21,8 @@ window.PROFILE = {
   basic: {
     nameCn:     "孙瑞",
     nameEn:     "Rui Sun",
-    // avatarUrl:  "https://api.dicebear.com/7.x/avataaars/svg?seed=SunRui&backgroundColor=b6e3f4",  // 网络随机选取的照片用来测试
-    avatarUrl:  "./images/SR.jpg",
+    avatarUrl:  "https://api.dicebear.com/7.x/avataaars/svg?seed=SunRui&backgroundColor=b6e3f4",  // 网络随机选取的照片用来测试
+    // avatarUrl:  "./images/SR.jpg",
     birth:      "1999/09/20",
     political:  "中共党员",
     hometown:   "陕西·榆林",
